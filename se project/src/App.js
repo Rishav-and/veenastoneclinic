f111b83619029d9dbc97c499768a8b78e61ffad9
Rishav-and <82319065+Rishav-and@users.noreply.github.com>
2022-11-06 21:52:15 +0530

    Add files via upload
    latest

---
+++ se project/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Home from "./component/Home";
+import Contact from "./component/Contact";
+function App() {
+  return (
+   <Home />
+   <Contact />
+  );
+}
+
+export default App;
